7f08a7dbf00f1496513e2cb01ab744dc99aefecc
Wojciech Wieclawski <wojtekwieclawski@gmail.com>
2021-08-15 02:21:55 +0200

    initial commit after setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const onResize = () => {
+    document.querySelector(":root").style.setProperty("--vh", window.innerHeight / 100 + "px");
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    onResize();
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center bg-green-300 h-screen-js">
+      <h1 className="text-9xl grid ">Cześć 🖐️</h1>
     </div>
   );
 }
